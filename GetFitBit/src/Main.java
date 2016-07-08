5e1d079b6d27142df0ac80437270b9232fe6134d
Dan <kramer425@gmail.com>
2016-07-08 11:39:44 -0400

    initial commit

---
+++ GetFitBit/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		while(true){
+			try{
+				String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0UEJYQlEiLCJhdWQiOiIyMjdWNTMiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNDk5NTIzOTI4LCJpYXQiOjE0Njc5ODc5Mjh9.W74YpdpGl9Nw66uwTWeZARtAE_3cR6UNNawtKKbKXOs";
+				URLReader.getData(token);
+			}catch(Exception e){
+				System.out.println(e.toString());
+			}
+		}
+	}
+
+}
