27d9307c7a2fe26de34513cae578db1a217ee01e
twyixstudios <twyixstudios@gmail.com>
2016-07-11 13:20:23 -0400

    Get activities

--- GetFitBit/src/application/DailyActivity.java
+++ GetFitBit/src/application/DailyActivity.java
@@ -1,60 +1,81 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import org.json.*;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class DailyActivity {
-	public static void getActivity(String uId,String aToken) throws Exception{
-		String simpledate = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
-	    
-		URL url = new URL("https://api.fitbit.com/1/user/" + uId + "/activities/date/" + simpledate + ".json");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("GET");
-        
-        connection.setRequestProperty("Authorization", " Bearer " + aToken);
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        
-        String data = in.readLine();
-        System.out.println(data);
-        final JSONObject object = new JSONObject(data);
-        final JSONObject summary = object.getJSONObject("summary");
-        
-        System.out.println(summary.getInt("activityCalories"));
-        System.out.println(summary.getInt("steps"));
-        System.out.println(summary.getInt("floors"));
-        
-       
-         
-        in.close();
+	
+	public static ArrayList<Activity> getActivities(String uId,String aToken, Date startDate, Date endDate){
+		LocalDate start = startDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate end = endDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		
+		ArrayList<Activity> activities = new ArrayList<Activity>();
+		
+		ArrayList<String> dateRange = new ArrayList<String>();
+		
+		for (LocalDate date = start; date.isBefore(end); date = date.plusDays(1)) {
+			dateRange.add(date.format(Main.DATE_TIME_FORMATTER));
+		}
+		
+		dateRange.parallelStream().forEach(date -> {
+			try{
+				URL url = new URL("https://api.fitbit.com/1/user/" + uId + "/activities/date/" + date + ".json");
+				
+		        BufferedReader in = new BufferedReader(new InputStreamReader(getInputStreamFromURL(url, aToken).getInputStream()));
+		        
+		        String data = in.readLine();
+		        System.out.println(data);
+		        in.close();
+	
+		        final JSONObject object = new JSONObject(data);
+		        final JSONObject summary = object.getJSONObject("summary");
+		        
+		        int restingHeartRate = 0;
+		        
+		        try{
+		        	restingHeartRate = getRestingDaily(uId, aToken, date);
+		        }catch(Exception e){
+		        }
+		        
+		        activities.add(new Activity(summary.getInt("activityCalories"), summary.getInt("steps"), summary.getInt("floors"), restingHeartRate));
+			}catch(Exception e){
+		     }
+		});
+		
+		return activities;
 	}
 	
-	public static void getResting(String uId,String aToken) throws Exception{
-		//String simpledate = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
-	    
-		URL url = new URL("https://api.fitbit.com/1/user/" + uId +"/activities/heart/date/today/1d.json");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("GET");
-        
-        connection.setRequestProperty("Authorization", " Bearer " + aToken);
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	private static int getRestingDaily(String uId,String aToken, String date) throws Exception{
+		URL url = new URL("https://api.fitbit.com/1/user/" + uId +"/activities/heart/date/" + date + "/1d.json");
+    
+        BufferedReader in = new BufferedReader(new InputStreamReader(getInputStreamFromURL(url, aToken).getInputStream()));
         
         String data = in.readLine();
-        System.out.println(data);
+        in.close();
+
         final JSONObject object = new JSONObject(data);
         final JSONArray heart = object.getJSONArray("activities-heart");
         final JSONObject heartObject = heart.getJSONObject(0);
         
-        System.out.println(heartObject.getJSONObject("value").getInt("restingHeartRate"));
+        return heartObject.getJSONObject("value").getInt("restingHeartRate");
+	}
+	
+	private static HttpURLConnection getInputStreamFromURL(URL url, String aToken) throws IOException{
+	    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("GET");
         
-        in.close();
+        connection.setRequestProperty("Authorization", " Bearer " + aToken);
+        return connection;
 	}
 }
--- GetFitBit/src/application/Main.java
+++ GetFitBit/src/application/Main.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -10,6 +14,12 @@ public class Main {
 	private final static ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
 	private final static int REFRESH_TIME = 1;
 	
+	public final static SimpleDateFormat SIMPLE_DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+	public final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	
+	private final static String exampleStartDate = "2016-07-09";
+	private final static String exampleEndDate = "2016-07-11";
+	
 	public static void main(String[] args) {
 		
 		Runnable getUserRunnable = () -> {
@@ -17,12 +27,13 @@ public class Main {
 			String id = auth.get(0)[1];
 			String token = auth.get(0)[0];
 			try {
-				DailyActivity.getActivity(id, token);
-				DailyActivity.getResting(id, token);
-				
-			} catch (Exception e) {
-				System.out.println(e.toString());
+				System.out.println("before daily");
+				ArrayList<Activity> example = DailyActivity.getActivities(id, token, SIMPLE_DATE_FORMATTER.parse(exampleStartDate), SIMPLE_DATE_FORMATTER.parse(exampleEndDate));
+				System.out.println(example.size());
+				example.stream().forEach(a -> System.out.println(a.toString()));
+			} catch (ParseException e) {
 			}
+			
 		};
 		
 		
