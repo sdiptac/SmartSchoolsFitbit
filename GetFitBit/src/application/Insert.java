0f3b0c7ead14b598243f3f004a38d06d652db26e
suc217 <sdipta.c@gmail.com>
2016-07-11 14:41:28 -0400

    "insert"

--- GetFitBit/src/application/Insert.java
+++ GetFitBit/src/application/Insert.java
@@ -1,5 +1,22 @@
 package application;
 
-public class Insert {
+import java.util.ArrayList;
 
+public class Insert {
+	public static void insertDailyActivity(String uId, ArrayList<Activity> activityArray){
+		int calories = 0;
+		String date = null;
+		int floors = 0;
+		int steps = 0;
+		int restingHeartRate = 0;
+		final String query = "insert into dailyActivity values "
+		
+		for (Activity e : activityArray){
+			calories = e.getActivityCalories();
+			date = e.getDate();
+			floors = e.getFloors();
+			steps = e.getSteps();
+			restingHeartRate = e.getRestingHeartRate();
+		}
+	}
 }
