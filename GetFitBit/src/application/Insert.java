61aa40d7189e55ce95b63fb0e09c3a0269de92db
suc217 <sdipta.c@gmail.com>
2016-07-12 12:53:12 -0400

    "insert"

--- GetFitBit/src/application/Insert.java
+++ GetFitBit/src/application/Insert.java
@@ -1,15 +1,16 @@
 package application;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Insert {
-	public static void insertDailyActivity(String uId, ArrayList<Activity> activityArray){
+	public static void insertDailyActivity(String uId, int deviceId, ArrayList<Activity> activityArray){
 		int calories = 0;
 		String date = null;
 		int floors = 0;
 		int steps = 0;
 		int restingHeartRate = 0;
-		final String query = "insert into dailyActivity values "
 		
 		for (Activity e : activityArray){
 			calories = e.getActivityCalories();
@@ -17,6 +18,21 @@ public class Insert {
 			floors = e.getFloors();
 			steps = e.getSteps();
 			restingHeartRate = e.getRestingHeartRate();
+		}
+		try{
+			Connector.connect();
+			final String query = "insert into dailyActivity values (?,?,?,?,?,?)";
+			PreparedStatement statement= Connector.connection.prepareStatement(query);
+			statement.setInt(1, deviceId);
+			statement.setInt(2, calories);
+			statement.setInt(3, steps);
+			statement.setInt(4, floors);
+			statement.setInt(5, restingHeartRate);
+			statement.setString(6, date);
+			statement.executeUpdate();
+			
+		} catch (SQLException e) {
+			System.out.println(e.toString());
 		}
 	}
 }
